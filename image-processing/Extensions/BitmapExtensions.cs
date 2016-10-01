1512bb9019b6e3929c0363c8bce17e7de7ed4eef
Константин Старцев < >
2016-10-02 02:52:27 +0500

    Fast bitmap

--- image-processing/Extensions/BitmapExtensions.cs
+++ image-processing/Extensions/BitmapExtensions.cs
@@ -7,18 +7,18 @@ namespace image_processing.Extensions
     {
         public static Bitmap ToGrayscale(this Bitmap bmp, Func<Color, byte> grayscaleFunction)
         {
-            using (var dbmp = new DirectBitmap(bmp.Width, bmp.Height))
+            var newBmp = (Bitmap)bmp.Clone();
+            using (var fastBitmap = newBmp.FastLock())
             {
-                for (var y = 0; y < dbmp.Height; y++)
+                for (var y = 0; y < fastBitmap.Height; y++)
                 {
-                    for (var x = 0; x < dbmp.Width; x++)
+                    for (var x = 0; x < fastBitmap.Width; x++)
                     {
-                        var color = bmp.GetPixel(x, y);
-                        var value = grayscaleFunction(color);
-                        dbmp[x, y] = Color.FromArgb(value, value, value);
+                        var value = grayscaleFunction(fastBitmap.GetPixel(x,y));
+                        fastBitmap.SetPixel(x, y, Color.FromArgb(value, value, value));
                     }
                 }
-                return (Bitmap)dbmp.Bitmap.Clone();
+                return newBmp;
             }
         }
     }
--- image-processing/Model.cs
+++ image-processing/Model.cs
@@ -15,20 +15,23 @@ namespace image_processing
 
         public double CalculatePSNR(Image a, Image b)
         {
-            var abmp = new Bitmap(a);
-            var bbmp = new Bitmap(b);
-            var sum = 0l;
-            for (var i = 0; i < 512; i++)
+            using (var fastBitmapA = (new Bitmap(a).FastLock()))
             {
-                for (var j = 0; j < 512; j++)
+                using (var fastBitmapB = (new Bitmap(b).FastLock()))
                 {
-                    sum += (int)Math.Pow(abmp.GetPixel(i, j).R - bbmp.GetPixel(i, j).R, 2);
-                    sum += (int)Math.Pow(abmp.GetPixel(i, j).G - bbmp.GetPixel(i, j).G, 2);
-                    sum += (int)Math.Pow(abmp.GetPixel(i, j).B - bbmp.GetPixel(i, j).B, 2);
+                    var sum = 0l;
+                    for (var i = 0; i < 512; i++)
+                    {
+                        for (var j = 0; j < 512; j++)
+                        {
+                            sum += (int)Math.Pow(fastBitmapA.GetPixel(i, j).R - fastBitmapB.GetPixel(i, j).R, 2);
+                            sum += (int)Math.Pow(fastBitmapA.GetPixel(i, j).G - fastBitmapB.GetPixel(i, j).G, 2);
+                            sum += (int)Math.Pow(fastBitmapA.GetPixel(i, j).B - fastBitmapB.GetPixel(i, j).B, 2);
+                        }
+                    }
+                    return 10 * Math.Log10(255 * 255 * 512.0 / sum * 512);
                 }
             }
-            double q = 3 * 255 * 255 * 512.0 / sum * 512;
-            return 10 * Math.Log10(q);
         }
     }
 }
